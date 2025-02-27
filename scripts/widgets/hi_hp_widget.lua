020d532cda5d558667d8e1a1a5a4c2dae2ecb2bb
Egor Morunov <generalissimusunpaleness@gmail.com>
2025-02-27 23:12:18 +0100

    use nineslice instead of image to get rid of scaling issues

--- scripts/widgets/hi_hp_widget.lua
+++ scripts/widgets/hi_hp_widget.lua
@@ -1,30 +1,36 @@
 local Text = require "widgets/text"
 local HiBaseWidget = require "widgets/hi_base_widget"
 local Widget = require "widgets/widget"
+local NineSlice = require "widgets/nineslice"
 
-local HP_INNER_SIZE_X = 284
-local HP_INNER_SIZE_Y = 24
-local IMAGE_SCALE = 0.5
+local HP_SIZE_X = 150
+local HP_SIZE_Y = 20
+local BOX_BG_PADDING = 6
+local BOX_INNER_PADDING = 6
+local BOX_BG_MIN_SIZE = 6
 local STATE_HOSTILE = 0
 local STATE_FRIEND = 1
 local STATE_NEUTRAL = 2
 local STATE_PLAYER = 3
 local FONT_SIZE = 50
+local FONT_MIN_SIZE = 10
 local TINT_HOSTILE = {0.75, 0.25, 0.25, 1}
 local TINT_FRIEND = {0.25, 0.75, 0.25, 1}
 local TINT_NEUTRAL = {0.75, 0.75, 0.75, 1}
 local TINT_PLAYER = {0.75, 0.25, 0.75, 1}
 
-local function GetHpScale(value)
+local function GetScaledValues(value)
     -- Let's say 0.25 scale will be at value <= 1 and 1 scale will be at value >= 200
     local cap_min = 1
     local cap_max = 20000
     local result_min = 0.25
     local result_max = 1
     local clamped_value = math.min(cap_max, math.max(math.abs(value), cap_min))
-    local result = math.log(clamped_value) / math.log(cap_max) * (result_max - result_min) + result_min
-    -- print("GetHpScale: ", math.log(clamped_value), " / ", math.log(cap_max), " * " , (result_max - result_min), " + ", result_min, " = ", result)
-    return result
+    local scale = math.log(clamped_value) / math.log(cap_max) * (result_max - result_min) + result_min
+    local scale_x = math.max(math.floor(HP_SIZE_X * scale), BOX_BG_MIN_SIZE)
+    local scale_y = math.max(math.floor(HP_SIZE_Y * scale), BOX_BG_MIN_SIZE)
+    local scale_font = math.max(FONT_SIZE * scale, FONT_MIN_SIZE)
+    return scale_x, scale_y, scale_font
 end
 
 local function GetHpWidgetState(target)
@@ -47,27 +53,29 @@ end
 local HiHpWidget = Class(HiBaseWidget, function(self, hp, max_hp)
     HiBaseWidget._ctor(self, "HiHpWidget")
     self.offset = Vector3(0, -20, 0)
-    self.scale = 1
+    self.hp_bar_size_x = HP_SIZE_X
+    self.hp_bar_size_y = HP_SIZE_Y
+    self.text_size = FONT_SIZE
     self.hp = 0
     self.state = STATE_NEUTRAL
-    self.image_bg = self:AddChild(Image("images/hp_bg.xml", "HpBg.tex"))
-    self.image_bg:SetScale(IMAGE_SCALE)
-    self.image = self:AddChild(Image("images/hp_white.xml", "HpWhite.tex"))
-    self.image:SetScale(IMAGE_SCALE)
-    self.text = self:AddChild(Text(BODYTEXTFONT, FONT_SIZE, math.floor(hp), { 1, 1, 1, 1 }))
-    self.text:SetPosition(0, -FONT_SIZE / 1.5)
+    self.box_bg = self:AddChild(NineSlice("images/hp_bg.xml"))
+    self.box_bg:SetSize(self.hp_bar_size_x - BOX_BG_PADDING, self.hp_bar_size_y - BOX_BG_PADDING)
+    self.box = self:AddChild(NineSlice("images/hp_white.xml"))
+    self.box:SetSize(self.hp_bar_size_x - BOX_INNER_PADDING, self.hp_bar_size_y - BOX_INNER_PADDING)
+    self.text = self:AddChild(Text(BODYTEXTFONT, self.text_size, math.floor(hp), { 1, 1, 1, 1 }))
+    self.text:SetPosition(0, -(self.hp_bar_size_y + self.text_size) * 0.5)
     self:SetOpacity(HI_SETTINGS.data.hp_bar_opacity)
     self:UpdateHp(hp, max_hp)
     self:UpdateWhilePaused(false)
 end)
 
 function HiHpWidget:SetOpacity(a)
-    self.image_bg:SetFadeAlpha(a)
-    self.image:SetFadeAlpha(a)
+    self.box_bg:SetFadeAlpha(a)
+    self.box:SetFadeAlpha(a)
 end
 
 function HiHpWidget:SetImageTint(tint)
-    self.image:SetTint(tint[1], tint[2], tint[3], tint[4])
+    self.box:SetTint(tint[1], tint[2], tint[3], tint[4])
 end
 
 function HiHpWidget:SetTarget(target)
@@ -76,10 +84,8 @@ function HiHpWidget:SetTarget(target)
 end
 
 function HiHpWidget:Kill()
-    self.image_bg:Kill()
-    if self.image ~= nil then
-        self.image:Kill()
-    end
+    self.box_bg:Kill()
+    self.box:Kill()
     self.text:Kill()
     Widget.Kill(self)
 end
@@ -93,16 +99,18 @@ function HiHpWidget:UpdateHp(hp, max_hp)
         print("HiHpWidget:UpdateHp: max_hp is 0")
         return
     end
-    if self.image ~= nil then
-        local ratio = hp / max_hp
-        self.image:SetPosition(HP_INNER_SIZE_X * 0.5 * (ratio - 1) * IMAGE_SCALE, 0, 0)
-        self.image:SetScale(ratio * IMAGE_SCALE, IMAGE_SCALE)
+    local x, y, text_size = GetScaledValues(max_hp)
+    if self.hp_bar_size_x ~= x or self.hp_bar_size_y ~= y then
+        self.hp_bar_size_x = x
+        self.hp_bar_size_y = y
+        self.text_size = text_size
+        self.box_bg:SetSize(self.hp_bar_size_x - BOX_BG_PADDING, self.hp_bar_size_y - BOX_BG_PADDING)
+        self.text:SetSize(self.text_size)
+        self.text:SetPosition(0, -(self.hp_bar_size_y + self.text_size) * 0.5)
     end
-    local new_scale = GetHpScale(max_hp)
-    if new_scale ~= self.scale then
-        self.scale = new_scale
-        self:SetScale(self.scale)
-    end
+    local hp_bar_filler_size_x = math.max((self.hp_bar_size_x - BOX_INNER_PADDING) * hp / max_hp, 0)
+    self.box:SetPosition((BOX_INNER_PADDING - self.hp_bar_size_x + hp_bar_filler_size_x) * 0.5, 0, 0)
+    self.box:SetSize(hp_bar_filler_size_x, self.hp_bar_size_y - BOX_INNER_PADDING)
 end
 
 function HiHpWidget:UpdateState(force)
