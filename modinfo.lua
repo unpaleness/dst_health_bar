489624faac6a64ab9073b1188f03ef7710895117
Egor Morunov <generalissimusunpaleness@gmail.com>
2025-12-25 15:29:28 +0100

    start v0.0.25

--- modinfo.lua
+++ modinfo.lua
@@ -1,13 +1,13 @@
 name = "Health indicators"
 description = [[
-Version: 0.0.24
+Version: 0.0.25
 Features:
 - health bars with dynamic colour coding
 - damage indicators
 - in-game settings
 ]]
 author = "unpaleness"
-version = "0.0.24"
+version = "0.0.25"
 
 steam_description = [[
 [b]A very base health and damage indicators[/b]
